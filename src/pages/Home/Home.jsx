753b5d7db378a4fc76da4baddbd6e0a422a3ac8a
sjohannknecht <sjohannknecht@techfak.uni-bielefeld.de>
2024-01-05 17:49:36 +0100

    Fix use of the useOutletContext in Home and in the Products test

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,7 @@
 import {useOutletContext} from "react-router-dom";
 
 function Home() {
-    const products = useOutletContext();
+    const {products} = useOutletContext();
     return <>
         <h1>Home</h1>
         {products.map(product => <p key={product.id}>{product.title}</p>)}
