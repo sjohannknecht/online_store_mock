0afd4d0e4affab39aa3b0a99d7453a0a9fd47e21
sjohannknecht <sjohannknecht@techfak.uni-bielefeld.de>
2024-01-05 17:52:48 +0100

    Extract content of Product page into another component

--- src/pages/Product/Product.jsx
+++ src/pages/Product/Product.jsx
@@ -1,20 +1,17 @@
 import {useOutletContext, useParams} from "react-router-dom";
+import ProductCard from "../../components/ProductCard/ProductCard";
 
 function Product() {
-    const products = useOutletContext();
+    const {products} = useOutletContext();
     const {id} = useParams();
     const product = products.find(product => product.id.toString() === id);
     if (!product) {
         return <p>Product not found</p>
     }
 
-    return <>
-        <h2>{product.title}</h2>
-        <img src={product.image} alt={product.title}></img>
-        <p>{product.price}</p>
-        <p>{product.description}</p>
-        <p>{product.category}</p>
-    </>;
+    return <section>
+        <ProductCard product={product}></ProductCard>
+    </section>
 }
 
 export default Product;
