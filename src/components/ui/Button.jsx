971fba658805043634ad5b02277ad5f323ba309f
sjohannknecht <sjohannknecht@techfak.uni-bielefeld.de>
2024-01-05 17:36:11 +0100

    Add className prop to Button and adjust its styling

--- src/components/ui/Button.jsx
+++ src/components/ui/Button.jsx
@@ -1,12 +1,13 @@
 import "./Button.css"
 import PropTypes from "prop-types";
 
-function Button({onClick, children, ...rest}) {
-    return <button onClick={onClick} {...rest}>{children}</button>;
+function Button({onClick, children, className, ...rest}) {
+    return <button onClick={onClick} className={["Button", className].join(" ")} {...rest}>{children}</button>;
 }
 
 Button.propTypes = {
     onClick: PropTypes.func,
+    className: PropTypes.string,
     children: PropTypes.node.isRequired,
 }
 
