ee6f612b644a89abb91c2d5de0c44ff4aa38debe
sjohannknecht <sjohannknecht@techfak.uni-bielefeld.de>
2024-01-05 18:25:10 +0100

    Add AddToCartWidget component and tests

---
+++ src/components/AddToCartWidget/AddToCartWidget.jsx
@@ -0,0 +1,56 @@
+import "./AddToCartWidget.css";
+import Button from "../ui/Button";
+import PropTypes from "prop-types";
+import {useOutletContext} from "react-router-dom";
+import {useState} from "react";
+
+function AddToCartWidget({product}) {
+    const [numberInput, setNumberInput] = useState(1);
+    const {dispatchShoppingCart} = useOutletContext();
+    const [addedToCartHistory, setAddedToCartHistory] = useState([]);
+
+    function handleAddToCart() {
+        dispatchShoppingCart({
+            type: "add",
+            productId: product.id,
+            quantity: numberInput,
+        })
+        setAddedToCartHistory(previous => [...previous, numberInput]);
+    }
+
+    function handleNumberInput(event) {
+        setNumberInput(event.target.value);
+    }
+
+    if (!product) {
+        return <div>Product not available.</div>
+    }
+
+    return <div className="AddToCartWidget">
+        <label htmlFor={product.id.toString()} className="for">Quantity:</label>
+        <input
+            id={product.id.toString()}
+            className="AddToCartWidget__input"
+            type="number"
+            onInput={handleNumberInput}
+            min="1"
+            value={numberInput}/>
+        <Button className="Button--secondary" onClick={handleAddToCart}>Add to Cart</Button>
+        {addedToCartHistory.map((element, index) => <p className="AddToCartWidget__success-message" key={index}>
+            {`${element} ${element === 1 ? "item" : "items"} added to cart`}
+        </p>)}
+    </div>
+}
+
+AddToCartWidget.propTypes = {
+    product: PropTypes.shape({
+        id: PropTypes.number,
+        title: PropTypes.string,
+        price: PropTypes.number,
+        category: PropTypes.string,
+        description: PropTypes.string,
+        image: PropTypes.string,
+    })
+}
+
+export default AddToCartWidget;
---
+++ src/components/__tests__/AddToCartWidget.test.jsx
@@ -0,0 +1,45 @@
+import {render, screen} from "@testing-library/react";
+import '@testing-library/jest-dom'
+import userEvent from '@testing-library/user-event'
+import AddToCartWidget from "../AddToCartWidget/AddToCartWidget";
+
+//mock the useOutletContext hook of react-router-dom cause the AddToCartWidget component uses the dispatchShoppingCart function
+const mockFunction = jest.fn();
+jest.mock('react-router-dom', () => ({
+    ...jest.requireActual('react-router-dom'),
+    useOutletContext: () => (
+        {
+            dispatchShoppingCart: mockFunction,
+        }
+    ),
+}));
+
+describe("AddToCartWidget component", () => {
+    const product = {"id":1,"title":"Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops","price":109.95,"description":"Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday","category":"men's clothing","image":"https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg","rating":{"rate":3.9,"count":120}};
+
+    test("dispatchShoppingCart function is called when button is clicked", async () => {
+        const user = userEvent.setup();
+        render(<AddToCartWidget product={product}></AddToCartWidget>);
+        await user.click(screen.getByRole("button"));
+        expect(mockFunction).toHaveBeenCalled();
+    })
+
+    test("dispatchShoppingCart function is called with correct arguments when button is clicked", async () => {
+        const user = userEvent.setup();
+        render(<AddToCartWidget product={product}></AddToCartWidget>);
+        await user.click(screen.getByRole("button"));
+        expect(mockFunction.mock.lastCall[0].type).toBe("add");
+        expect(mockFunction.mock.lastCall[0].productId).toBe(1);
+        expect(mockFunction.mock.lastCall[0].quantity).toBe(1);
+    })
+
+    test("dispatchShoppingCart function is called with correct arguments when number input is changed and button is clicked", async () => {
+        const user = userEvent.setup();
+        render(<AddToCartWidget product={product}></AddToCartWidget>);
+        await user.type(screen.getByRole("spinbutton"), "5");
+        await user.click(screen.getByRole("button"));
+        expect(mockFunction.mock.lastCall[0].type).toBe("add");
+        expect(mockFunction.mock.lastCall[0].productId).toBe(1);
+        expect(mockFunction.mock.lastCall[0].quantity).toBe("15");
+    })
+})
