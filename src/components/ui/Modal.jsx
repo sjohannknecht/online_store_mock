41aec9b291cf1b9ff6e4eff50d1310dcea3929b6
sjohannknecht <sjohannknecht@techfak.uni-bielefeld.de>
2024-01-06 13:22:22 +0100

    Create Modal component and tests

---
+++ src/components/ui/Modal.jsx
@@ -0,0 +1,23 @@
+import "./Modal.css";
+import PropTypes from "prop-types";
+import {forwardRef} from "react";
+import Button from "./Button";
+
+const Modal= forwardRef(function Modal({children}, ref) {
+
+    function handleOk() {
+        ref.current.close();
+    }
+
+
+    return <dialog ref={ref}>
+        <p>{children}</p>
+        <Button className="Button--secondary" autoFocus onClick={handleOk}>Ok</Button>
+    </dialog>
+});
+
+Modal.propTypes = {
+    children: PropTypes.node,
+}
+
+export default Modal;
