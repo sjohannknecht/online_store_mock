560b515a98e58df75f7abaf83188d0063028820c
sjohannknecht <sjohannknecht@techfak.uni-bielefeld.de>
2024-01-06 15:21:29 +0100

    Add a placeholder message on the Homepage

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,10 @@
-import {useOutletContext} from "react-router-dom";
+import Card from "../../components/ui/Card";
+import "./Home.css";
 
 function Home() {
-    const {products} = useOutletContext();
     return <>
         <h1>Home</h1>
-        {products.map(product => <p key={product.id}>{product.title}</p>)}
+        <Card className="Home__message">This is a prototype for an online store. You can browse the mock products and add them to your shopping cart. Note that you cannot really purchase anything here.</Card>
     </>;
 }
 
